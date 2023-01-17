5763980200883d871d9cd7242897d43df191b8d6
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-01-17 18:26:36 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,52 @@
+// comentarion de una sola linea
+
+/* 
+comentario
+de
+varias
+lineas
+*/
+
+// let edad; //declaracion de una variable recomendada
+// edad = 16; //asignacion de una variable
+
+// var nombre = "andres"; //decalaracion de una variable no recomendada
+// var apellido = 'ruiz';
+
+// const CURSO = "JavaScript";
+
+// console.log("este es el valor de la variable", edad);
+
+// nombreBonitoGrande => CamelCase
+// nombre_bonito_grande => SnakeCase
+// NombreBonitoGrande => PascalCase
+
+// let numeroUno = 4;
+// let numeroDos = 6;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+
+// console.log("el resultado de la suma es: ", resultadoSuma);
+// console.log("el resultado de la resta es: ", resultadoResta);
+
+// let saludo = "Hola";
+// let nombre = "Camila";
+
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+
+// let nombre = prompt("Ingrese su nombre");
+// console.log("el nombre de la persona es", nombre);
+
+// let nombre = prompt("Ingrese su nombre");
+// alert("el nombre de la persona es " + nombre);
+
+// let edad = 56;
+// let nombre = "mariano";
+
+// console.log(typeof(edad));
+// console.log(typeof(nombre));
+
+let variable = parseFloat(prompt("Ingrese algo"));
+alert(typeof(variable));
