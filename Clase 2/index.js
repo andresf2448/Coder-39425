1ee51629cad1801d33394a4d9de3a42eab638c16
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-01-19 17:54:00 -0500

    Clase 2

---
+++ Clase 2/index.js
@@ -0,0 +1,138 @@
+/* 
+estructura de el condicional if
+
+if(condicion){
+  codigo a ejecutar en caso de que la condicion sea verdadera
+}
+*/
+
+// if (true) {
+//   console.log("entramos");
+// }
+
+// console.log("finalizamos");
+
+// if (5 > 3) {
+//   console.log("5 es mayor que 3");
+// }
+
+/* 
+= asignacion (let nombre = "camilo";)
+== comparación pero solo evalua el valor ("3" == 3 =>> true)
+=== compararción evaluando tanto valor como tipo de dato ("3" === 3 =>> false)
+*/
+
+// let numero = 6;
+
+// if(numero === 5){
+//   console.log("el numero es 5");
+// }
+
+// let agua = prompt("Hay agua");
+
+// if(agua === "si"){
+//   alert("no salgo");
+// }
+
+// let color = prompt("ingrese un color");
+
+// if(color === "rojo"){
+//   alert("escribiste rojo");
+// }
+
+// if(color === "azul"){
+//   alert("escribiste azul");
+// }
+
+/* 
+estructura del if else
+
+if(condicion){
+  codigo a ejecutar cuando la condicion es verdadera
+}else{
+  codigo a ejecutar cuando la condicion no es verdadera
+}
+*/
+
+// let respuesta = prompt("Realizaste la tarea");
+
+// if(respuesta === "si"){
+//   alert("Puedes salir a jugar");
+// }else{
+//   alert("no puede salir a jugar");
+// }
+
+// let nombre = prompt("Ingrese su nombre");
+
+// if(nombre === ""){
+//   alert("No ingresaste el usuario");
+// }else{
+//   alert("Bienvenido " + nombre);
+// }
+
+// let respuesta = prompt("Está lloviendo???");
+
+// if(respuesta === "si"){
+//   alert("No salgo");
+// }else{
+//   alert("Si salgo");
+// }
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+
+// if(edad < 18){
+//   alert("No puedes entrar a la fiesta");
+// }else{
+//   alert("Puedes entrar a la fiesta");
+// }
+
+/* 
+estructura del if else if ... else
+
+if(condicion1){
+  codigo a ejecutar cuando la condicion1 es verdadera
+}else if(condicion2){
+  codigo a ejecutar cuando la condicion2 es verdadera
+}else if(condicion3){
+  codigo a ejecutar cuando la condicion3 es verdadera
+}else{
+  codigo a ejecutar cuando ninguna de las condiciones es verdadera
+}
+*/
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+
+// if(edad < 14){
+//   alert("No puede entrar a la fiesta");
+// }else if(edad < 18){
+//   alert("Puedes entrar a la fiesta con un adulto");
+// }else if(edad < 50){
+//   alert("Puedes entrar a la fiesta");
+// }else{
+//   alert("Eres muy mayor, no puedes entrar");
+// }
+
+// let numero = 5;
+// let verdad = true;
+// let falsedad = numero > 10;
+
+// console.log(verdad);
+// console.log(falsedad);
+
+// let edad = parseInt(prompt("Ingrese la edad"))
+
+// if (edad == NaN) {
+//   alert("No ingreso un numero en el campo");
+// } else {
+//   if (edad < 18) {
+//     alert("No puedes entrar a la fiesta");
+//   } else {
+//     alert("Puedes entrar a la fiesta");
+//   }
+// }
+
+// if (isNaN(nombre)) {   by Luis
+//   console.log("Bienvenido " + nombre);
+// }else {
+//   console.log("Ingrese un nombre válido");
+// }
